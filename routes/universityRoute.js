9f11db61897abb62c5dd2e65bb2a22c2098e2699
Pharel <pharelborfo@gmail.com>
2025-06-06 15:35:33 +0100

    crud

---
+++ routes/universityRoute.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import * as universityController from '../controllers/universityController.js';
+
+const universityRouter = Router();
+
+
+universityRouter.post('/', universityController.createUniversity);
+universityRouter.get('/', universityController.getAllUniversities);
+universityRouter.get('/:id', universityController.getUniversityById);
+universityRouter.patch('/:id', universityController.updateUniversity);
+universityRouter.delete('/:id', universityController.deleteUniversity);
+
+
+
+
+export default universityRouter;

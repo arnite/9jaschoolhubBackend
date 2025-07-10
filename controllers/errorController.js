f7e0e9e1d3a23405db934736c8f8678f5ada0b43
Agada Daniel <unogwuagada@gmail.com>
2025-07-10 18:45:45 +0100

    Fix error controller and add Jest unit tests

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -24,7 +24,7 @@ const sendErrorProd = (err, res) => {
 };
 
 
-export default (err, req, res, next) => {
+module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
